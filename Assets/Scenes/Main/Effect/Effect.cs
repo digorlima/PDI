1e70fc2f6a93a5f9d5cd537a17f39e3c3000ae07
Digorlima <38363044+digorlima@users.noreply.github.com>
2019-11-20 15:14:16 -0300

    UI

---
+++ Assets/Scenes/Main/Effect/Effect.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Effect : MonoBehaviour
+{
+    enum Effects { ADD, SUB };
+
+    // Variáveis Públicas ------
+    public GameObject allImages;
+    // -------------------------
+
+    // Variáveis Privadas -------------------------
+    private int effect;
+    private bool state = false;
+    private bool buffer = false;
+    private int size = -1;
+    private List<Image> images = new List<Image>();
+    // --------------------------------------------
+
+    private void Start() {
+        buffer = state;
+    }
+
+    private void Update() {
+        if (images.Count == size) {
+            SetState(false);
+            DoEffect();
+        }
+
+        if (buffer != state) {
+            Select();
+        } 
+
+        buffer = state;
+    }
+
+    // Minhas Funções ----------------------------------------------------
+    private void Select() {
+        Component[] toggles = allImages.GetComponentsInChildren<Toggle>();
+
+        foreach (Toggle joint in toggles) {
+            joint.interactable = state;
+        }
+    }
+
+    public void AddImage(Image image) {
+        images.Add(image);
+    }
+
+    public void DoEffect() {
+        switch (effect) {
+            case (int)Effects.ADD:
+                Add();
+                break;                        
+        }
+    }
+
+    // -----------------
+    private void Add() {
+        
+        
+        foreach(Image joint in images) {
+            joint.sprite.texture
+        }
+    }
+    // -----------------
+
+    // Gets and Sets -----------------
+    public bool GetState() {
+        return state;
+    }
+
+    public void SetState(bool state) {
+        this.state = state;
+    }
+    
+    public int GetSize() {
+        return size;
+    }
+
+    public void SetSize(int size) {
+        this.size = size;
+    }
+
+    public int GetEffect() {
+        return effect;
+    }
+
+    public void SetEffect(int effect) {
+        this.effect = effect;
+    }
+    // --------------------------------
+
+    // -------------------------------------------------------------------
+}
---
+++ Assets/Scenes/Main/OpenFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using Doozy.Engine.UI;
+
+public class OpenFile : MonoBehaviour
+{
+    public GameObject content;
+    public GameObject viewport;
+    public GameObject imagePreset;
+
+    public Effect effect;
+
+    public float imageSize;
+
+    public void Open()
+    {
+        string path = EditorUtility.OpenFilePanel("Open a image", "", "png;*jpg;*jpeg;*");
+        var fileContent = File.ReadAllBytes(path);
+        
+        Texture2D tex = new Texture2D(0, 0);
+        tex.LoadImage(fileContent);
+        tex.Apply();
+
+        float big;
+        if (tex.width > tex.height)
+        {
+            big = tex.width;
+        }
+        else
+        {
+            big = tex.height;
+        }
+        float scale = imageSize / big;
+
+        GameObject newObject = Instantiate(imagePreset);
+        newObject.transform.SetParent(content.transform, false);
+
+        GameObject image = newObject.transform.GetChild(0).gameObject;
+
+        image.GetComponent<Image>().sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        ((RectTransform)newObject.transform).sizeDelta = new Vector2 (tex.width * scale, tex.height * scale);
+        newObject.transform.localScale = new Vector3(1, 1);
+
+        image.GetComponent<Toggle>().interactable = effect.GetState();
+        image.GetComponent<UIToggle>().OnClick.OnToggleOn.Event.AddListener(delegate { ValueChanged(image.GetComponent<Image>()); });
+
+        if (viewport.GetComponent<Mask>() == null)
+        {
+            viewport.AddComponent<Mask>().showMaskGraphic = false;
+            viewport.AddComponent<Image>();
+        }
+    }
+
+    void ValueChanged(Image image) {
+        effect.AddImage(image);
+    }
+}
